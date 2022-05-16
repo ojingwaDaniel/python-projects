00f44f25a800aba3a36f5b7283c6600d35e6b3e0
ojingwaDaniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-05-17 00:47:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 perimeter = ('hours')
 calculation_Of_Unit = 24
-number = input('Enter a number:\n')
+
 def firstFunction(num_days):
     if num_days > 0 :
-     print (f"{num_days} days make {calculation_Of_Unit * num_days} {perimeter}")
-    elif num_days == 0:
-     print('You entered 0 please enter a positve number to convert')
-    else:
-        print('you entered a negative number NO conversion for u')
-def validation():
-   while True:
-      if number.isdigit():
-        Users_input = int(number)
+        print(f"{num_days} days make {calculation_Of_Unit * num_days} {perimeter}")
 
-        calculated_value = firstFunction(int(Users_input))
-        print(calculated_value)
-      else:
-          print('Your input is not  valid')
-
-validation()
-
+def validation_and_execution():
+    try:
+        Users_input_number = int(num_of_days_element)
+        if Users_input_number > 0 :
+            calculated_value = firstFunction(Users_input_number)
+            return (calculated_value)
+        elif Users_input_number == 0:
+            return ('You entered 0 please enter a positive number to convert')
+        else:
+            return ('you entered a negative number NO conversion for u')
+    except ValueError:
+        return ('you entered an invalid number')
+users_input = ''
+while users_input != 'exit':
+    users_input = input('Enter a list a number of days separated by spaces and i will convert it for u :\n')
+    for num_of_days_element in users_input.split():
+        validation_and_execution()
 
 
 
